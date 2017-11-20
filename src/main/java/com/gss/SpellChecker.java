35e0effc20ffe1c4cb92993c83017d3a412f2604
Sergii Gnuchykh <Sergii_Gnuchykh@epam.com>
2017-11-20 22:27:14 +0200

    Dependency injection

---
+++ src/main/java/com/gss/SpellChecker.java
@@ -0,0 +1,11 @@
+package com.gss;
+
+public class SpellChecker {
+    public SpellChecker() {
+        System.out.println("Inside SpellChecker constructor.");
+    }
+
+    public void checkSpelling() {
+        System.out.println("Inside checkSpelling.");
+    }
+}

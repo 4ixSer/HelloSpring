deb1202b1af4c1e0d4eacef5d3d80ae3578c992c
Sergii Gnuchykh <Sergii_Gnuchykh@epam.com>
2017-12-03 22:51:24 +0200

    AOP

---
+++ src/main/java/com/gss/test4/Logging.java
@@ -0,0 +1,20 @@
+package com.gss.test4;
+
+public class Logging {
+    public void beforeAdvice(){
+        System.out.println("Going to setup student profile.");
+    }
+
+    public void afterAdvice(){
+        System.out.println("Student profile has been setup.");
+    }
+
+    public void afterReturningAdvice(Object retVal){
+        System.out.println("Returning :"+ retVal.toString());
+    }
+
+    public void afterThrowingAdvice(IllegalAccessException ex){
+        System.out.println("There has been an exception" );
+
+    }
+}
